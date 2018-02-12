8910b741e6375aca0c80eaead9a2ab74c031282a
Ihab Hussein <ihab.hussein@plentymarkets.com>
2018-02-12 18:23:41 +0100

    - initial data

---
+++ src/Providers/FesServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace EbayFes\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+/**
+ * Class FesServiceProvider
+ * @package EbayFes\Providers
+ */
+class FesServiceProvider extends ServiceProvider {
+
+    public function register() {
+
+    }
+
+}
